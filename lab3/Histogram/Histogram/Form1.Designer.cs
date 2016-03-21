1f4b845fb142c3e45d4d58c5c40a69a1101eec3a
Dmitry Babin <dimababin@gmail.com>
2016-03-21 19:23:41 +0300

    lab3 initial release

---
+++ lab3/Histogram/Histogram/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Histogram
+{
+    partial class Histogram
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.myPictureBox = new System.Windows.Forms.PictureBox();
+            this.myZedGraphControl = new ZedGraph.ZedGraphControl();
+            this.loadButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myPictureBox
+            // 
+            this.myPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.myPictureBox.Name = "myPictureBox";
+            this.myPictureBox.Size = new System.Drawing.Size(424, 586);
+            this.myPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.myPictureBox.TabIndex = 0;
+            this.myPictureBox.TabStop = false;
+            this.myPictureBox.Click += new System.EventHandler(this.myPictureBox_Click);
+            // 
+            // myZedGraphControl
+            // 
+            this.myZedGraphControl.Location = new System.Drawing.Point(442, 12);
+            this.myZedGraphControl.Name = "myZedGraphControl";
+            this.myZedGraphControl.ScrollGrace = 0D;
+            this.myZedGraphControl.ScrollMaxX = 0D;
+            this.myZedGraphControl.ScrollMaxY = 0D;
+            this.myZedGraphControl.ScrollMaxY2 = 0D;
+            this.myZedGraphControl.ScrollMinX = 0D;
+            this.myZedGraphControl.ScrollMinY = 0D;
+            this.myZedGraphControl.ScrollMinY2 = 0D;
+            this.myZedGraphControl.Size = new System.Drawing.Size(1004, 615);
+            this.myZedGraphControl.TabIndex = 1;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 604);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Load Image";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // Histogram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1458, 639);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.myZedGraphControl);
+            this.Controls.Add(this.myPictureBox);
+            this.Name = "Histogram";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox myPictureBox;
+        private ZedGraph.ZedGraphControl myZedGraphControl;
+        private System.Windows.Forms.Button loadButton;
+    }
+}
+
---
+++ lab3/Histogram/Histogram/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Histogram
+{
+    public partial class Histogram : Form
+    {
+        public Histogram()
+        {
+            InitializeComponent();
+         
+
+        }
+
+        private Image image;
+        private bool imageReady = false;
+        
+        
+    private void myPictureBox_Click(object sender, EventArgs e)
+        {
+            string path = null;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.JPEG;*.PNG";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                imageReady = true;
+                path = file.FileName;
+                image = Image.FromFile(path);
+                CalculateColors(image);
+                myPictureBox.Image = image;
+            }
+        }
+        private void CalculateColors(Image image)
+        {
+            Bitmap bitmap = new Bitmap(image);
+            double[] red = new double[256];
+            double[] green = new double[256];
+            double[] blue = new double[256];
+           
+
+            double redTotal = 0;
+            double greenTotal = 0;
+            double blueTotal = 0;
+            BigInteger iterator = 0;
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    Color pixel = bitmap.GetPixel(i, j);
+     
+                    red[pixel.R]++;
+                    green[pixel.G]++;
+                    blue[pixel.B]++;
+                    redTotal += pixel.R;
+                    greenTotal += pixel.G;
+                    blueTotal += pixel.B;
+                }
+            }
+
+            int pixelsTotal = bitmap.Width * bitmap.Height;
+            DrawGraph(red, green, blue, redTotal / pixelsTotal, greenTotal / pixelsTotal, blueTotal / pixelsTotal);
+        }
+        private void DrawGraph(double[] redValues, double[] greenValues, double[] blueValues,double red,double green, double blue)
+        {
+            // Получим панель для рисования
+            GraphPane pane = myZedGraphControl.GraphPane;
+
+            // Размеры шрифтов для разных элементов графика
+            int labelsXfontSize = 5;
+            int labelsYfontSize = 5;
+
+            int titleXFontSize = 10;
+            int titleYFontSize = 10;
+
+            int legendFontSize = 10;
+
+            int mainTitleFontSize = 10;
+
+            // Установим размеры шрифтов для меток вдоль осей
+            pane.XAxis.Scale.FontSpec.Size = labelsXfontSize;
+            pane.YAxis.Scale.FontSpec.Size = labelsYfontSize;
+
+            // Установим размеры шрифтов для подписей по осям
+            pane.XAxis.Title.FontSpec.Size = titleXFontSize;
+            pane.YAxis.Title.FontSpec.Size = titleYFontSize;
+
+            // Установим размеры шрифта для легенды
+            pane.Legend.FontSpec.Size = legendFontSize;
+
+            // Установим размеры шрифта для общего заголовка
+            pane.Title.FontSpec.Size = mainTitleFontSize;
+            pane.Title.FontSpec.IsUnderline = true;
+            // Очистим список кривых на тот случай, если до этого сигналы уже были нарисованы
+            pane.CurveList.Clear();
+
+            pane.Title.Text = "Histogram";
+            pane.Title.FontSpec.FontColor = Color.Black;
+
+            int itemscount = 256;
+
+            Random rnd = new Random();
+
+            // Подписи под столбиками
+            double[] names = new double[itemscount];
+
+
+            // Заполним данные
+            for (int i = 0; i < itemscount; i++)
+            {
+                names[i] = i;
+            }
+
+            // Создадим кривую-гистограмму
+            // Первый параметр - название кривой для легенды
+            // Второй параметр - значения для оси X, т.к. у нас по этой оси будет идти текст, а функция ожидает тип параметра double[], то пока передаем null
+            // Третий параметр - значения для оси Y
+            // Четвертый параметр - цвет
+            BarItem curve = pane.AddBar("Red: "+red, names, redValues, Color.Red);
+            BarItem curve2 = pane.AddBar("Green: "+green, names, greenValues, Color.Green);
+            BarItem curve3 = pane.AddBar("Blue: "+blue, names, blueValues, Color.Blue);
+            curve.Bar.Fill.Type = FillType.Solid;
+            curve2.Bar.Fill.Type = FillType.Solid;
+            curve3.Bar.Fill.Type = FillType.Solid;
+
+            // Сделаем границы столбцов невидимыми
+            curve.Bar.Border.IsVisible = false;
+            curve2.Bar.Border.IsVisible = false;
+            curve3.Bar.Border.IsVisible = false;
+
+            pane.BarSettings.MinClusterGap = 0.0f;
+            pane.XAxis.Scale.Min = -1;
+            pane.XAxis.Scale.Max = 256;
+            pane.YAxis.Scale.Min = 0;
+            pane.YAxis.Scale.Max = redValues.Max();
+            pane.XAxis.Title.Text = "Color value";
+            pane.XAxis.Color = Color.Black;
+            pane.YAxis.Title.Text = "Pixel count";
+            pane.YAxis.Color = Color.Black;
+
+
+
+            // Вызываем метод AxisChange (), чтобы обновить данные об осях. 
+            myZedGraphControl.AxisChange();
+
+            // Обновляем график
+            myZedGraphControl.Invalidate();
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            string path = null;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.JPEG;*.PNG";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                imageReady = true;
+                path = file.FileName;
+                image = Image.FromFile(path);
+                CalculateColors(image);
+                myPictureBox.Image = image;
+            }
+            
+        }
+    }
+}
